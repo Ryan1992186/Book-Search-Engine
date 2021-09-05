0bb36b352e41c3a2d681f02ebba56c56a1d02185
Ryan Hillier <ryan1992186@gmail.com>
2021-09-05 16:12:25 -0500

    modify signupform change variables to utilize mutations

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,13 +1,14 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-
+import { LOGIN_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/react-hooks'
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  const [login] = useMutation(LOGIN_USER)
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
@@ -27,17 +28,15 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await login({
+        variables: { ...userFormData }
+      });
+      console.log(data);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      Auth.login(data.login.token);
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
+    } catch (e) {
+      console.error(e);
       setShowAlert(true);
     }
 
@@ -47,7 +46,7 @@ const LoginForm = () => {
       password: '',
     });
   };
-
+  
   return (
     <>
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
